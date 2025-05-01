13234b35b62f0a2b6d3acdd47981f3701bd37071
guiquan-1 <tanguiquan@gmail.com>
2025-05-01 10:03:45 +0900

    Added code for basic data visualization

--- starting_file.R
+++ starting_file.R
@@ -1,1 +1,21 @@
-Hello
+
+# Import the files
+df_tour_era   <- read.csv("data/CI_TOUR_ERA_CHARTR_INFO_20250331.csv", stringsAsFactors = FALSE)
+print(df_tour_era)
+
+#Basic Data Visualization
+# View the first few rows
+head(df_tour_era)
+
+# View the structure (columns, types)
+str(df_tour_era)
+
+# Summary statistics (numeric + categorical)
+summary(df_tour_era)
+
+# Dimensions (rows and columns)
+dim(df_tour_era)
+
+# Column names
+names(df_tour_era)
+
