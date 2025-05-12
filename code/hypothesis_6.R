b4ef8bf6557bdc93be0f482ed23b4ba5a263f352
guiquan-1 <tanguiquan@gmail.com>
2025-05-13 01:10:56 +0900

    analysis for hypo 6

---
+++ code/hypothesis_6.R
@@ -0,0 +1,79 @@
+### hypothesis 6: Income affects the destination of the trip
+
+# Variables required: Income and Travel
+#Income: HSHLD_INCOME_DGREE_NM
+#Destination: TOUR_CTPRVN_NM
+
+# We see that income had 모름 entries when checking for missing entries earlier
+
+# remove all rows with 모름 for HSHLD_INCOME_DGREE_NM
+df_hypo_6 <- df_tour_era %>%
+  filter(HSHLD_INCOME_DGREE_NM != "모름")
+
+#because of funny entries
+valid_income_levels <- c("300만원 미만", "300~500만원 미만", "500~700만원 미만", "700만원 이상")
+df_hypo_6 <- df_hypo_5 %>%
+  filter(HSHLD_INCOME_DGREE_NM %in% valid_income_levels)
+
+
+#converting to factors
+df_hypo_6 <- df_hypo_6 %>%
+  mutate(
+    HSHLD_INCOME_DGREE_NM = factor(HSHLD_INCOME_DGREE_NM, 
+                                   levels = c("300만원 미만", "300~500만원 미만", 
+                                              "500~700만원 미만", "700만원 이상"),
+                                   ordered = TRUE),
+    SEXDSTN_FLAG_CD = factor(SEXDSTN_FLAG_CD),        # Gender
+    AGRDE_FLAG_NM   = factor(AGRDE_FLAG_NM, 
+                             levels = c("20대", "30대", "40대", "50대", "60대 이상"), 
+                             ordered = TRUE),
+    MRRG_AT_NM      = factor(MRRG_AT_NM),             # Marital status
+    CHLDRN_TY_NM    = factor(CHLDRN_TY_NM),           # Child type
+    OCCP_NM         = factor(OCCP_NM),                # Occupation
+    TOUR_PURPS_NM   = factor(TOUR_PURPS_NM,
+                             levels = c(
+                               "식도락 (지역 특색/제철 음식)",
+                               "놀이/테마공원-온천 등 즐기기",
+                               "자연 풍경 감상 (산/ 바다 등)",
+                               "휴식",
+                               "문화-예술 즐기기 (미술관/ 공연 등)",
+                               "역사-유적 감상 (유적지/ 박물관 등)",
+                               "취미-운동 활동 (등산/ 낚시/ 골프 등)",
+                               "친지/친구/친척 만나기",
+                               "도시 경관 감상 (건축물/ 거리 등)",
+                               "쇼핑",
+                               "축제-행사 참여",
+                               "기타"
+                             )
+    ),
+    TOUR_PD_VALUE   = factor(TOUR_PD_VALUE),          # Travel duration
+    TOUR_COM_NMPR_NM = factor(TOUR_COM_NMPR_NM),      # Travel group size
+    COM_ONE_TY      = factor(COM_ONE_TY),             # Companion types
+    COM_TWO_TY      = factor(COM_TWO_TY),
+    COM_THREE_TY    = factor(COM_THREE_TY),
+    
+    TOUR_CTPRVN_NM = factor(TOUR_CTPRVN_NM,
+                            levels = c(
+                              "강원도", "경상남도", "전라북도", "부산시", "제주도", "서울시", "경기도",
+                              "대구시", "충청남도", "충청북도", "전라남도", "경상북도", "광주시", 
+                              "대전시", "인천시", "울산시"
+                            ))
+  )
+
+ggplot(df_hypo_6, aes(x = HSHLD_INCOME_DGREE_NM, fill = TOUR_CTPRVN_NM)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Destination by Household Income Level",
+       x = "Income Level", y = "Proportion of Respondents",
+       fill = "Province Visited") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
+
+
+
+# to check - dont have to submit
+# Contingency table
+table_income_dest <- table(df_hypo_6$HSHLD_INCOME_DGREE_NM, df_hypo_6$TOUR_CTPRVN_NM)
+# Chi-squared test
+chisq.test(table_income_dest)
+
